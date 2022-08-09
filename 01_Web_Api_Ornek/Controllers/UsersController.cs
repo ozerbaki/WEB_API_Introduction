c804303b088856e06f38e931224d250305e5484a
Baki Ozer <ozerbaki@hotmail.com.tr>
2022-08-09 22:43:46 +0300

    FakeData ile Api Controlleri

---
+++ 01_Web_Api_Ornek/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using _01_Web_Api_Ornek.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_Web_Api_Ornek.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "Get Users";
+        //}
+
+
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "Get Users2" + id;
+        //}
+
+        private List<User> users = Fake.FakeData.GetUsers(100);
+
+
+        /// <summary>
+        /// User List
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<User> Get()
+        {
+            return users;
+        }
+
+        /// <summary>
+        /// Select a User
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")] //Select
+        public User Get(int id)
+        {
+            return users.FirstOrDefault(x => x.Id == id);
+        }
+
+        /// <summary>
+        /// Insert a User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost] //Insert
+        public User Post([FromBody] User user)
+        {
+            users.Add(user);
+            return user;
+        }
+
+        /// <summary>
+        /// Update a User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var editUser = users.FirstOrDefault(x => x.Id == user.Id);
+            editUser.FirstName = user.FirstName;
+            editUser.LastName = user.LastName;
+            editUser.Address = user.Address;
+
+            return user;
+        }
+
+        /// <summary>
+        /// Delete a User
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deleteUser = users.FirstOrDefault(x => x.Id == id);
+
+            users.Remove(deleteUser);
+        }
+    }
+}
---
+++ 01_Web_Api_Ornek/Fake/FakeData.cs
@@ -0,0 +1,29 @@
+﻿using _01_Web_Api_Ornek.Models;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_Web_Api_Ornek.Fake
+{
+    public static class FakeData
+    {
+        private static List<User> _users;
+
+        public static List<User> GetUsers(int number)
+        {
+            if (_users == null)
+            {
+            _users = new Faker<User>()
+                .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+                .RuleFor(x => x.LastName, f => f.Name.LastName())
+                .RuleFor(x => x.Address, f => f.Address.FullAddress())
+                .Generate(number);
+            }
+
+            return _users;
+        }
+    }
+}
